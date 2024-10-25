ef74e3c00f2e1daaad842776a6a15c185b1f84bc
Md. Firoz Kabir <mdfirozkabir.31@gmail.com>
2024-10-25 12:13:50 +0600

    changed errorpage image

--- src/Shared/ErrorPages/ErrorPage.jsx
+++ src/Shared/ErrorPages/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import errorImg from '../../assets/404.gif'
 
 const ErrorPage = () => {
     return (
 
         <div className="mx-auto items-center justify-center">
-            <div style={{ backgroundImage: `url("https://i.ibb.co/gjwgsBy/404.png")`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className='h-screen flex justify-center items-center'>
+            <div style={{ backgroundImage: `url("${errorImg}")`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className='h-screen flex justify-center items-center'>
                 <div className='justify-center items-center text-center'>
                     <h1 className='font-bold text-7xl text-center mb-6 text-red-500'> Oops </h1>
                     <Link className="btn bg-red-500 hover:bg-red-400 border-0 text-white mt-2" to="/">Go back Home</Link>
